13abcd7dedc11c0dd777d1efb28a0ea87b28f3ae
Haben1797 <Haben97.M@Gmail.com>
2025-06-11 12:12:23 +0300

    added stuff to Person.java (test)

--- Models/Person.java
+++ Models/Person.java
@@ -1,4 +1,12 @@
 package Models;
 public class Person {
-    
+    int id;
+    String name;
+    String email;
+
+    public Person(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
 }
