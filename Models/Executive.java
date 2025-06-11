8f5e85fd33d1960d8da30e3ba5dfa71987543cfe
Hallelujah <HallelujahEzra@gmail.com>
2025-06-11 12:57:45 +0300

    will add methods

--- Models/Executive.java
+++ Models/Executive.java
@@ -1,4 +1,13 @@
 package Models;
-public class Executive {
+public class Executive extends Person{
+    private String managersList;
+    private String projectsList;
+    private String employeeList;
+    Executive (String managersList, String projectsList, String employeeList, String name, String ID, String email) {
+        super(name, ID, email);
+        this.managersList = managersList;
+        this.projectsList = projectsList;
+        this.employeeList = employeeList;
+    }
     
 }
