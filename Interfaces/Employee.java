f9ecd53767751cc49cb511c368eb0c95fc72d442
Kertina <kertinabekele@gmail.com>
2025-06-11 16:25:17 +0300

    Update Employee.java

--- Interfaces/Employee.java
+++ Interfaces/Employee.java
@@ -36,4 +36,23 @@ public class Employee extends person {
         this.projectIds = projectIds;
     }
     
+ public static void main(String[] args) {
+        List<String> tasks = new ArrayList<>();
+        tasks.add("Task1");
+        tasks.add("Task2");
+
+        List<String> projects = new ArrayList<>();
+        projects.add("Project1");
+        projects.add("Project2");
+
+        // Create an Employee object
+        Employee employee = new Employee("John Doe", "E123", "M456", tasks, projects);
+
+        // Print employee details
+        System.out.println("Employee Name: " + employee.getName());
+        System.out.println("Employee ID: " + employee.getId());
+        System.out.println("Manager ID: " + employee.getManagerId());
+        System.out.println("Assigned Tasks: " + employee.getAssignedTaskIds());
+        System.out.println("Project IDs: " + employee.getProjectIds());
+    }
 }
