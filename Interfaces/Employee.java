c5477da7c6440d51c8cf77c122efb5cb6f40a3df
Kertina <kertinabekele@gmail.com>
2025-06-11 15:50:30 +0300

    Update Employee.java

--- Interfaces/Employee.java
+++ Interfaces/Employee.java
@@ -1,6 +1,39 @@
 package Interfaces;
 
 public class Employee extends person {
-    
+     private String managerId; // The ID of their direct manager
+    private List<String> assignedTaskIds; // IDs of specific tasks assigned to this employee
+    private List<String> projectIds; // IDs of projects this employee is actively involved in
+//constructor
+    ublic Employee(String name, String id, String managerId, List<String> assignedTaskIds, List<String> projectIds) {
+        super(name, id); // Call the constructor of the Person class
+        this.managerId = managerId;
+        this.assignedTaskIds = assignedTaskIds;
+        this.projectIds = projectIds;
+    }
+ // Getters and Setters
+    public String getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(String managerId) {
+        this.managerId = managerId;
+    }
+
+    public List<String> getAssignedTaskIds() {
+        return assignedTaskIds;
+    }
+
+    public void setAssignedTaskIds(List<String> assignedTaskIds) {
+        this.assignedTaskIds = assignedTaskIds;
+    }
+
+    public List<String> getProjectIds() {
+        return projectIds;
+    }
+
+    public void setProjectIds(List<String> projectIds) {
+        this.projectIds = projectIds;
+    }
     
 }
